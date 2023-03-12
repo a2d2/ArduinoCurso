bd2fcadd81e3f35659442c1361c9a85c1bd24030
r2d2 <arturo.galvis@hotmail.com>
2023-03-12 15:15:07 -0500

    Services commented on App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import About from './components/About';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Services from './components/Services';
+// import Services from './components/Services';
 import Tours from './components/Tours';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Services />
+      {/* <Services /> */}
       <Tours />
       <Footer />
     </>
